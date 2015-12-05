08f084dc35b0298db3a3a22cfb6cefaeb2fef7ef
Miguel Cunha <miguelfcunha@gmail.com>
2015-12-05 17:22:02 +0000

    Testing toastnotifications

--- api/models/User.js
+++ api/models/User.js
@@ -4,6 +4,7 @@
 * @description :: TODO: You might write a short summary of how this model works and what it represents here.
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
+var Promise = require('bluebird');
 
 module.exports = {
 
@@ -28,11 +29,26 @@ module.exports = {
   },
 
   beforeCreate: function(user, cb) {
-    User.destroy({phoneID: user.phoneID}).exec(function deleteCB(err) {
-      console.log('The record has been deleted');
-    });
 
-    cb(null, user);
+    Promise.all([
+      User.destroy({phoneID: user.phoneID})
+    ])
+      .spread(function(destroyeduser) {
+
+        if (destroyeduser[0] != null) {
+          UserStock.destroy({owner: destroyeduser[0].id}).exec(function (err, destroyed) {
+
+          });
+        }
+      })
+      .catch(function(err){
+        console.log(err);
+      })
+      .done(function(){
+        console.log("");
+        cb(null,user);
+      });
+
   }
 };
 
--- config/cron.js
+++ config/cron.js
@@ -1,41 +1,108 @@
 var Promise = require('bluebird');
-var  AppID = 'ms-app://s-1-15-2-7517944-2102956592-847385009-1620487831-2475186946-440704142-3222778260';
-var  AppSecret = 'cdJpJ6kmSPpAYBPRECs2mFtYvuTZBGyy';
+var wns = require('wns');
 
-var createApps = function(){
+var options = {
+  client_id: 'ms-app://s-1-15-2-7517944-2102956592-847385009-1620487831-2475186946-440704142-3222778260',
+  client_secret: 'cdJpJ6kmSPpAYBPRECs2mFtYvuTZBGyy'
+};
+
+
+function getNewData(){
+  var getStocks = Promise.promisify(UserStock.query);
+  var stockvalues = {};
+  var pro = Promise.defer();
 
   Promise.all([
-
+    getStocks('SELECT DISTINCT name FROM UserStock')
   ])
-    .spread(function(){
-      //do nothing
+    .spread(function(results) {
+      var stocks = results.rows;
+      var baseURL = 'http://finance.yahoo.com/d/quotes?f=l1&s=';
+
+      async.each(stocks, function(stock, callback) {
+
+        var request = require('request');
+        request(baseURL + stock.name, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            stockvalues[stock.name] = body;
+            callback();
+          } else {
+            callback("ERRO!");
+          }
+        });
+
+      }, function(err){
+        if( err ) {
+          console.log('A URL failed to process');
+          pro.reject();
+        } else {
+          pro.resolve(stockvalues);
+        }
+      });
     })
     .catch(function(err){
       console.log(err);
     })
     .done(function(){
-      console.log("");
+
+    });
+
+  return pro.promise;
+}
+
+var reloadData = function(){
+
+  sails.log("Updating values......\n\n");
+
+  Promise.all([
+    getNewData()
+  ])
+    .spread(function(stockHash) {
+
+      UserStock.find({}).populate('owner')
+        .then(function(stockarray){
+
+          stockarray.forEach(function(entry) {
+            var realValue = stockHash[entry.name];
+
+            // tile notification
+
+            if( realValue > entry.max || realValue < entry.min){
+              //toast notification
+              console.log("ENVIAR NOTIFICACAO: " + entry.name + " PARA: " + entry.owner.channelURL);
+
+              wns.sendToastText02(entry.owner.channelURL, "Pediu para ser notificado sobre " + entry.name +":", "Valor atual: " + realValue , options, function (error, result) {
+                if (error)
+                  console.error(error);
+                else
+                  console.log(result);
+              });
+
+
+
+            }
+
+          });
+
+        })
+        .catch(function(err){
+          sails.log.error(err);
+        });
+
+    })
+    .catch(function(err){
+      console.log(err);
+    })
+    .done(function(){
     });
 
 }
 
 
-
-
-
-
-
-
 module.exports.cron = {
-  /*dailyJob: {
-    schedule: '0 0 * * *',
+  minuteJob: {
+    schedule: '* * * * *',
 
-    onTick: createDailyTimeTable
-  },*/
-
-  oneTimeJob: {
-    schedule: new Date(new Date().getTime() + 7000),
-
-    onTick: createApps
+    onTick: reloadData
   }
 };
